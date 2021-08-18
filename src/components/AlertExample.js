f2528cfe05cab9d78d3b031fbf505fa9a841c5e1
Rachel Eiting <reiting1@gmail.com>
2021-08-18 17:10:18 -0500

    Added cases for all state values to reducer provided onChange for all inputs

--- src/components/AlertExample.js
+++ src/components/AlertExample.js
@@ -1,11 +1,51 @@
+import { useAlertReducer } from "./AlertManager";
 
 const AlertExample = () => {
+  const [state, dispatch] = useAlertReducer()
+  const { timeLimit, text, link, alertType, alertTitle } = state
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
   return (
-    <form onSubmit>
-      Text: <input type='text' name='text'></input>
-      Link: <input type='text' name='link'></input>
-      Time Limit: <input type='text' name='timeLimit'></input>
-      Alert Type: <input type='text' name='alertType'></input>
+    <form onSubmit={onSubmit}>
+      TimeLimit: <input type='text' value={timeLimit} onChange={(e) =>
+        dispatch({
+          type: 'timeLimit',
+          fieldName: 'timeLimit',
+          payload: e.target.value,
+        })
+      } />
+      Text: <input type='text' value={text} onChange={(e) =>
+        dispatch({
+          type: 'text',
+          fieldName: 'text',
+          payload: e.target.value,
+        })
+      } />
+      Link: <input type='text' value={link} onChange={(e) =>
+        dispatch({
+          type: 'link',
+          fieldName: 'link',
+          payload: e.target.value,
+        })
+      } />
+      Alert Type: <input type='text' value={alertType} onChange={(e) =>
+        dispatch({
+          type: 'alertType',
+          fieldName: 'alertType',
+          payload: e.target.value,
+        })
+      } />
+      Alert Title: <input type='text' value={alertTitle} onChange={(e) =>
+        dispatch({
+          type: 'alertTitle',
+          fieldName: 'alertTitle',
+          payload: e.target.value,
+        })
+      } />
       <input type='submit' value='submit'></input>
     </form>
   )
--- src/components/AlertManager.js
+++ src/components/AlertManager.js
@@ -1,7 +1,36 @@
+import { useReducer } from "react"
 
-const AlertManager = () => {
+let uniqueId =
+  Math.floor(Math.random() * 1000)
 
-
+const initialState = {
+  timeLimit: '10000',
+  text: '',
+  link: '',
+  alertType: '',
+  id: uniqueId,
+  alertTitle: ''
 }
 
-export default AlertManager
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'timeLimit':
+      return { ...state, }
+    case 'text':
+      return { ...state, }
+    case 'link':
+      return { ...state }
+    case 'alertType':
+      return { ...state }
+    case 'alertTitle':
+      return { ...state }
+    default:
+      return state
+  }
+}
+
+export const useAlertReducer = () => {
+  return useReducer(reducer, initialState);
+};
+
+
